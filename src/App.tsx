e7d7cbb4ed1424d52f895989f8be6a94fa933700
Dev-Andejecruher <antonio.cruz@clupp.com.mx>
2025-02-08 21:09:04 -0600

    feat: :art: agregar componentes principales y configuración inicial de la aplicación

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import Component from "@/pages/component-showcase";
+import DashboardPage from "@/components/layouts/Sidebard"
+
+function App() {
+  return (
+    <DashboardPage />
+  )
+}
+
+export default App
